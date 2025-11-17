024effdaa86dd9a1b8c4cbf232de670dccf39f64
Omni-VoidTime <paigekunze40@gmail.com>
2025-11-17 00:01:41 -0800

    Backend for calendar
    I was only able to do the query portion, it might be necessary to change the calendar html into a calendar php but for now they are separate

---
+++ backend/calendar.php
@@ -0,0 +1,26 @@
+<?php
+include("DataBase.php");
+include("DB.php");
+$result = $mysqli->query("SELECT * FROM appointments");
+
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $start = new DateTime($row["start_date"]);
+        $end = new DateTime($row["end_date"]);
+
+        while ($start <= $end) {
+            $eventsFromDB[] = [
+                "id" => $row["id"],
+                "title" => "{$row['course_name']} - {$row['instructor_name']}",
+                "date" => $start->format('Y-m-d'),
+                "start" => $row["start_date"],
+                "end" => $row["end_date"],
+                "start_time" => $row["start_time"],
+                "end_time" => $row["end_time"],
+            ];
+            $start->modify('+1 day');
+        }
+    }
+}
+$mysqli->close();
+?>
